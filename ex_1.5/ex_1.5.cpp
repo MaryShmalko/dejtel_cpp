0bbe4443273dbb6158b81d33e7f5c0469e65cb48
Mary Sh <maryshmalko@gmail.com>
2021-03-09 20:14:27 +0300

    пункт b is done

--- ex_1.5/ex_1.5.cpp
+++ ex_1.5/ex_1.5.cpp
@@ -1,9 +1,13 @@
 ﻿#include <iostream>
+#include <Windows.h>
 
 using namespace std;
 
 int main()
 {
+	SetConsoleOutputCP(CP_UTF8);
 	int c, thisAVariable, number;
+	cout << u8"Введите число: \n";
 	return 0;
 }
+
