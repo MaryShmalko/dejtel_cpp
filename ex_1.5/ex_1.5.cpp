c5d3247b53f0311ffc4daa76f54c1c4d51f603e2
Mary Sh <maryshmalko@gmail.com>
2021-03-09 20:34:13 +0300

    Пункт C был завершен

--- ex_1.5/ex_1.5.cpp
+++ ex_1.5/ex_1.5.cpp
@@ -6,8 +6,10 @@ using namespace std;
 int main()
 {
 	SetConsoleOutputCP(CP_UTF8);
+	int age;
 	int c, thisAVariable, number;
-	cout << u8"Введите число: \n";
+	cout << u8"Введите число: " ;
+	cin >> age;
 	return 0;
 }
 
